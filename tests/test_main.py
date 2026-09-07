d7eb299ddb9465b101076e8cb82fa76c02e86647
Akshaya Madas <298586420+AkshayaMadas@users.noreply.github.com>
2026-09-07 19:54:17 +0530

    Add initial API test

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_get_jobs():
+    response = client.get("/jobs/")
+    assert response.status_code == 401
